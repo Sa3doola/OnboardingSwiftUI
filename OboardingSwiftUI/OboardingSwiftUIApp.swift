c460862280298c76e2a23aef9c755c2ccdaeb5f3
Saad Sherif <saadsherif02@gmail.com>
2023-03-18 21:09:11 +0200

    Initial Commit

---
+++ OboardingSwiftUI/OboardingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OboardingSwiftUIApp.swift
+//  OboardingSwiftUI
+//
+//  Created by Saad Sherif on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct OboardingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
